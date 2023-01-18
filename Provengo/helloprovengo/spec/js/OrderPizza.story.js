3d36fa9f84ed7711b34300715d00d3b0b1d7cf2c
shirasorek <105981318+shirasorek@users.noreply.github.com>
2023-01-18 20:45:19 +0200

    Update OrderPizza.story.js

--- Provengo/helloprovengo/spec/js/OrderPizza.story.js
+++ Provengo/helloprovengo/spec/js/OrderPizza.story.js
@@ -2,20 +2,24 @@
 
 /**
  * This story opens a new browser window, goes to google.com, and searches for a pizza.
- */
-story('Search for a pizza on Google', function () {
-  let s = new SeleniumSession().start('https://google.com')
-  s.composeQuery({ text: 'Pizza' })
-  s.startSearch()
+
+*/
+
+story('Student submits Assignment', function () {
+  // the "with" statement makes it redundant to write "s." before each call to a defined event (like the story above)
+  with (new SeleniumSession().start('http://localhost/')) {
+
+     login({user: 'student', password: '1234Shira#'})
+     accessCourse()
+     submitAssignment()
+
+  }
 })
 
-/**
- * This story opens a new browser window, goes to google.com, and searches for a pizza using the "I Feel Lucky" feature.
- */
-story('Search using feeling lucky', function () {
-  // the "with" statement makes it redundant to write "s." before each call to a defined event (like the story above)
-  with (new SeleniumSession().start('https://google.com')) {
-    composeQuery({ text: 'Pizza' })
-    feelLucky()
+story('Teacher deletes Assignment', function(){
+  with (new SeleniumSession().start('http://localhost/')){
+  login({user: 'yakir', password: '1234Yakir#'})
+  accessCourse()
+  deleteAssignment()
   }
 })
